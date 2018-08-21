3f8d62ae4c6f86ef3ff3bf5b5e9ec3bbbf72370b
Zhujinyong <1019029168@qq.com>
2018-08-21 15:42:46 +0800

    none

--- HtmlToImagePdf/Controllers/HtmlController.cs
+++ HtmlToImagePdf/Controllers/HtmlController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Lib.HtmlToPdfImage;
+﻿using Lib.HtmlToPdfImage;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Web.HtmlToImagePdf.Controllers
